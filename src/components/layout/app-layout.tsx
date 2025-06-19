af88cdbc06a70cd4130802654cee280847e4c26d
Arslan Tekayev <turkzuk@gmail.com>
2025-06-19 20:56:10 +0000

    head navbar is overlapping with interface, fix these ugly overlapping

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -38,7 +38,7 @@ export function AppLayout({ children }: { children: ReactNode }) {
             </div>
             <UserNav />
           </header>
-          <main className="flex-1 overflow-y-auto p-6 md:p-8">
+          <main className="flex-1 overflow-y-auto pt-16 px-6 pb-6 md:px-8 md:pb-8">
             {children}
           </main>
         </SidebarInset>
